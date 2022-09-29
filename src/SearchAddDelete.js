210d9a9e8c0ab32cfefd062757cf699617c6cf0e
carmen <58992828+cgbl-90@users.noreply.github.com>
2022-09-29 15:58:06 +0200

    Summary
    1. Set search
    2. Improved syntax (arrow  functions)

--- src/SearchAddDelete.js
+++ src/SearchAddDelete.js
@@ -3,34 +3,63 @@ import data from "./fakeMovieService.json";
 import "./App.css";
 
 export default function SearchAddDelete() {
-  let [searchResult, setSearchResult] = useState({
-    title: " ",
-    genre: " ",
-    rate: 0,
-  });
+  let [searchID, setSearchID] = useState(5);
+  let [inputValue, setInputValue] = useState(" ");
+  let [includeExclude] = useState([0, 1, 2, 6, 7, 8]);
+
+  function searchMovie(event) {
+    event.preventDefault();
+    data.map(
+      (value, index) => inputValue === value.title && setSearchID(index)
+    );
+  }
 
   return (
     <main>
       <div className="searchBox">
         <form>
-          <input type="text" />
-          <button>SEARCH</button>
+          <input
+            type="text"
+            onChange={(event) => setInputValue(event.target.value.trim())}
+          />
+          <button onClick={searchMovie}>SEARCH</button>
         </form>
         <div className="resultBox">
-          <h3>TITLE: {searchResult.title}</h3>
-          <h3>GENRE: {searchResult.genre}</h3>
-          <h3>RATE: {searchResult.rate}</h3>
+          <h3>TITLE: {data[searchID].title}</h3>
+          <h3>GENRE: {data[searchID].genre.name}</h3>
+          <h3>RATE: {data[searchID].rate}</h3>
         </div>
+        <br />
+        <button
+          onClick={() =>
+            includeExclude.indexOf(data[searchID]) === -1 &&
+            includeExclude.push(data[searchID])
+          }
+        >
+          ADD
+        </button>
       </div>
       <div className="listBox">
-        <h3>MY ORDER</h3>
+        <h1>MY ORDER</h1>
         <h4> TITLE | GENRE | RATE </h4>
         {data.map((listItem, index) => {
-          return (
-            <h4 id={index}>
-              {listItem.title} | {listItem.genre.name} | {listItem.rate}
-            </h4>
-          );
+          if (includeExclude.indexOf(index) !== -1)
+            return (
+              <span className="list">
+                <h4 className="movie-list" id={index}>
+                  {listItem.title} | {listItem.genre.name} | {listItem.rate}
+                </h4>
+                <button
+                  className="btn-delete"
+                  onClick={(index) =>
+                    includeExclude.indexOf(index) !== -1 &&
+                    includeExclude.delete(includeExclude.indexOf(index))
+                  }
+                >
+                  DELETE
+                </button>
+              </span>
+            );
         })}
       </div>
     </main>
