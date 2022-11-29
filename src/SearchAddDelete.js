b6d744a835ceaaf2e265ae32a11eb5a9388dd01e
carmen <58992828+cgbl-90@users.noreply.github.com>
2022-11-29 13:49:53 +0100

    Summary
    - delete/add movies

--- src/SearchAddDelete.js
+++ src/SearchAddDelete.js
@@ -1,17 +1,22 @@
-import { useState, useEffect } from "react";
-import data from "./fakeMovieService.json";
+import { useState } from "react";
+import movie_list from "./fakeMovieService.json";
 import "./App.css";
 
 export default function SearchAddDelete() {
-  let [searchID, setSearchID] = useState(5);
   let [inputValue, setInputValue] = useState(" ");
-  let [includeExclude] = useState([0, 1, 2, 6, 7, 8]);
+  let [includeExclude] = useState([
+    "5b21ca3eeb7f6fbccd471821",
+    "5b21ca3eeb7f6fbccd47181f",
+    "5b21ca3eeb7f6fbccd471816",
+  ]);
+  let [movie, setMovie] = useState([]);
 
   function searchMovie(event) {
     event.preventDefault();
-    data.map(
-      (value, index) => inputValue === value.title && setSearchID(index)
-    );
+    movie_list.map((listItem) => {
+      console.log(listItem.title.includes(inputValue));
+      listItem.title.includes(inputValue) && setMovie(listItem);
+    });
   }
 
   return (
@@ -23,46 +28,36 @@ export default function SearchAddDelete() {
             onChange={(event) => setInputValue(event.target.value.trim())}
           />
           <button onClick={searchMovie}>SEARCH</button>
+          <div className="resultBox">
+            <h3>TITLE: {movie.title}</h3>
+            <h3>RATING: {movie.rate}</h3>
+          </div>
+          <br />
+          <button onClick={includeExclude.push(movie._id)}>ADD</button>
         </form>
-        <div className="resultBox">
-          <h3>TITLE: {data[searchID].title}</h3>
-          <h3>GENRE: {data[searchID].genre.name}</h3>
-          <h3>RATE: {data[searchID].rate}</h3>
-        </div>
-        <br />
-        <button
-          onClick={() =>
-            includeExclude.indexOf(data[searchID]) === -1 &&
-            includeExclude.push(data[searchID])
-          }
-        >
-          ADD
-        </button>
       </div>
       <div className="listBox">
         <h1>MY ORDER</h1>
         <h4> TITLE | GENRE | RATE </h4>
-        {useEffect(() => {
-          data.map((listItem, index) => {
-            if (includeExclude.indexOf(index) !== -1)
-              return (
-                <span className="list">
-                  <h4 className="movie-list" id={index}>
-                    {listItem.title} | {listItem.genre.name} | {listItem.rate}
-                  </h4>
-                  <button
-                    className="btn-delete"
-                    onClick={(index) =>
-                      includeExclude.indexOf(index) !== -1 &&
-                      includeExclude.delete(includeExclude.indexOf(index))
-                    }
-                  >
-                    DELETE
-                  </button>
-                </span>
-              );
-          });
-        }, [searchID])}
+        {movie_list.map((listItem, index) => {
+          if (includeExclude.includes(listItem._id))
+            return (
+              <span className="list">
+                <h4 className="movie-list" id={index}>
+                  {listItem.title} | {listItem.genre.name} | {listItem.rate}
+                </h4>
+                <button
+                  className="btn-delete"
+                  onClick={includeExclude.find(
+                    (element) =>
+                      element === listItem._id && delete includeExclude[element.indexOf]
+                  )}
+                >
+                  DELETE
+                </button>
+              </span>
+            );
+        })}
       </div>
     </main>
   );
